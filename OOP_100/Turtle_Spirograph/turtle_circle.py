b1c55718c23324682bf18808e7259111e0377db8
NickPantev <nickpantev33@gmail.com>
2022-10-06 14:05:07 +0300

    create Turtle Spirograph

---
+++ OOP_100/Turtle_Spirograph/turtle_circle.py
@@ -0,0 +1,25 @@
+import turtle as t
+import random
+from data import rainbow_colours
+
+
+tim = t.Turtle()
+tim.shape("turtle")
+
+tim.color("red", "green")
+tim.speed("fastest")
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random.choice(rainbow_colours))
+        tim.circle(-100)
+        current_heading = tim.heading()
+        tim.setheading(current_heading + size_of_gap)
+
+
+draw_spirograph(5)
+
+
+
+screen = t.Screen()
+screen.exitonclick()
