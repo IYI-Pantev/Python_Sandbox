1de782d6d7ecb29008b30dd4d93aff5e6fcb1d3e
NickPantev <nickpantev33@gmail.com>
2022-10-15 21:20:28 +0300

    create track record for highest score

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -32,13 +32,13 @@ while game_is_on:
         food.refresh()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
 
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
--- Snake_Game/scoreboard.py
+++ Snake_Game/scoreboard.py
@@ -11,6 +11,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highest_score = int(data.read())
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -20,8 +22,12 @@ class Scoreboard(Turtle):
     def score_refresh(self):
         self.clear()
         self.score += 1
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Score: {self.score} Highest score: {self.highest_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg=f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highest_score}")
+        self.score = -1
+        self.score_refresh()
