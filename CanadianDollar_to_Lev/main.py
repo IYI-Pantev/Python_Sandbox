703f2d92b84995014d9fe711ea646d5da8fa8b84
NickPantev <nickpantev33@gmail.com>
2023-01-21 16:27:19 +0200

    first GUI

---
+++ CanadianDollar_to_Lev/main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+
+window = Tk()
+
+window.title("CA$ to BGN")
+window.minsize(width=320, height=220)
+window.config(padx=25, pady=25)
+
+# Label equals to...
+
+expl_label = Label(text="is equal to")
+expl_label.config(padx=15, pady=15)
+expl_label.grid(column=0, row=1)
+
+# Entry
+
+entry = Entry()
+entry.config(width=10)
+
+entry.grid(column=1, row=0)
+
+# Label bgn equals
+bgn_label = Label(text="0")
+bgn_label.config(padx=15, pady=15)
+bgn_label.grid(column=1, row=1)
+
+
+# Button calculate
+def calculate():
+    cad = float(entry.get())
+    lev = round(cad * 1.35, 2)
+    bgn_label.config(text=f"{lev}")
+
+
+button = Button(text="Calculate", command=calculate)
+button.config(padx=10, pady=10)
+
+button.grid(column=1, row=2)
+
+# Label CAD
+
+cad_label = Label(text="Canadian Dollar")
+cad_label.config(padx=10, pady=10)
+cad_label.grid(column=2, row=0)
+
+# Label BGN
+lev_label = Label(text="Bulgarian Lev")
+lev_label.config(padx=10, pady=10)
+lev_label.grid(column=2, row=1)
+
+window.mainloop()
